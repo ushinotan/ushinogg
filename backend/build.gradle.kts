cbfb9cebe1f2efc9c2b9c95d8572f009cb128db3
牛乃タン <beeftongue551.vtuber@gmail.com>
2025-10-21 20:38:46 +0900

    up: kotlin-springプラグインのバージョンを更新
    kotlin("plugin.spring")のバージョンを1.9.20から2.2.20に更新しました。これにより、最新の機能やバグ修正を利用可能になります。他の依存関係には変更はありません。

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "2.2.20"
-    kotlin("plugin.spring") version "1.9.20"
+    kotlin("plugin.spring") version "2.2.20"
     id("com.google.devtools.ksp") version "2.2.20-2.0.4"
 }
 
