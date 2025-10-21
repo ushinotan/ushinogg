dc3f130c69442f5c0e3d48d75c9d16b9079d1a89
牛乃タン <beeftongue551.vtuber@gmail.com>
2025-10-21 20:35:19 +0900

    up: 依存関係とKotlinコンパイラ設定の更新

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,11 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.2.20"
     kotlin("plugin.spring") version "1.9.20"
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.4"
 }
 
 group = "com.ushinogg"
@@ -25,32 +26,33 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    
+    implementation("ch.qos.logback:logback-core:1.5.19")
+
     // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    
+
     // Komapper
-    val komapperVersion = "1.16.0"
+    val komapperVersion = "5.6.0"
     implementation("org.komapper:komapper-spring-boot-starter-jdbc:$komapperVersion")
     implementation("org.komapper:komapper-dialect-postgresql-jdbc:$komapperVersion")
     ksp("org.komapper:komapper-processor:$komapperVersion")
-    
+
     // PostgreSQL
     runtimeOnly("org.postgresql:postgresql")
-    
+
     // Discord JDA
-    implementation("net.dv8tion:JDA:5.0.0-beta.18")
-    
+    implementation("net.dv8tion:JDA:6.1.0")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
